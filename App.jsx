0f91cf70613446b59a7713d247c93c544ac3facd
Diana Rut <93566502+DIANARUTGARCIA@users.noreply.github.com>
2023-04-28 08:16:26 -0600

    first commit

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App(){
+  return (
+    <SafeAreaView >
+
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
